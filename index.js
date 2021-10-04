0d28a79e48874d5bfabb0a14fea4cf9936c3d74c
nersus15 <fathur.ashter15@gmail.com>
2021-10-04 19:19:08 +0800

    change api for total cases and deaths

--- index.js
+++ index.js
@@ -4,16 +4,31 @@ const totalVacinated = document.getElementById('totalVacinated');
 const totalPartialVacinated = document.getElementById('totalPartialVacinated');
 
 async function showData() {
-  const response_cases = await fetch('https://covid-api.mmediagroup.fr/v1/cases?country=India');
-  const response_vaccines = await fetch('https://covid-api.mmediagroup.fr/v1/vaccines?country=India');
-  const data_cases = await response_cases.json();
-  const data_vaccines = await response_vaccines.json();
-
-  totalCases.innerText = data_cases.All.confirmed;
-  totalDeaths.innerText = data_cases.All.deaths;
-  totalVacinated.innerText = data_vaccines.All.people_vaccinated;
-  totalPartialVacinated.innerText = data_vaccines.All.people_partially_vaccinated;
-
+  // const response_cases = await fetch('https://covid-api.mmediagroup.fr/v1/cases?country=India');
+  // const response_vaccines = await fetch('https://covid-api.mmediagroup.fr/v1/vaccines?country=India');
+  // const data_cases = await response_cases.json();
+  // const data_vaccines = await response_vaccines.json();
+  let response = await getDataByCountry('India');
+  render(response);
+  setInterval(async function(){
+      response = await getDataByCountry('India');
+      render(response);
+  }, 60000);
+}
+async function render(data){
+  const stat = await data.latest_stat_by_country[0];
+  totalCases.innerHTML = stat.active_cases;
+  totalDeaths.innerHTML = stat.total_deaths;
+}
+function getDataByCountry(keyword){
+  const options = {
+      "headers": {
+          "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+          "x-rapidapi-key": "beefe7b6eamsh027aa7179884c8ap135e99jsnfc04f913872c"
+      }  
+  }
+  const url = `https://coronavirus-monitor.p.rapidapi.com/coronavirus/latest_stat_by_country.php?country=${keyword}`;
+  return fetch(url, options).then(res => res.json()).then(data => Promise.resolve(data)).catch(err => Promise.reject(err));
 }
 
 showData();
